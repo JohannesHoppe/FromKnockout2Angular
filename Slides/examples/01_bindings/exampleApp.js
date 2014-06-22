f48945e88dd0f844463897fbed5c94d779e5b711
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2014-06-22 16:41:49 +0200

    bug fixed

---
+++ Slides/examples/01_bindings/exampleApp.js
@@ -0,0 +1,9 @@
+﻿angular.module('ExampleApp', [])
+    .controller('ExampleController', function ($scope) {
+
+        $scope.model = {
+            Title: "Remember",
+            Message: "the milk"
+        }
+        
+    });
