7ffb9500ca221656f2d51c5aeb1303aada765685
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2014-06-23 19:39:09 +0200

    working on templating / custom direcitves ex

---
+++ Slides/examples/02_templating/angular.js
@@ -0,0 +1,31 @@
+﻿angular.module('ExampleApp', [])
+
+    .directive('sticky-note', function () {
+        return {
+            // directive only matches elements now
+            restrict: 'E',  
+            replace: true,
+            // creates a new isolated scope 
+            // '=' means bi-directional / '@' means the variable will be copied (cloned)
+            scope: { 
+                Title: '@title',
+                Message: '@message',
+            },
+            template:
+                '<div class="sticky_note">' +
+                '    <div>' +
+                '        <h1>{{Title}}</h1>' +
+                '        <p>{{Message}}</p>' +
+                '    </div>' +
+                '</div>'
+        }
+    })
+
+    .controller('ExampleController', function ($scope) {
+
+        $scope.model = {
+            Title: "Remember",
+            Message: "the milk"
+        }
+        
+    });
---
+++ Slides/examples/02_templating/knockout.js
@@ -0,0 +1,11 @@
+﻿$(function () {
+
+    var ViewModel = function () {
+        this.Title = ko.observable('Remember');
+        this.Message = ko.observable('the milk');
+    };
+
+    var viewmodel = new ViewModel();
+    ko.applyBindings(viewmodel);
+
+});
