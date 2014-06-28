c447497905dd21d9a4736da00aad6c5d115838a2
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2014-06-28 11:44:54 +0200

    work in progress

---
+++ Slides/examples/03_modules/knockout.viewModel.js
@@ -0,0 +1,11 @@
+﻿require(['jquery', 'knockout', 'domReady!'], function ($, ko) {
+
+    var ViewModel = function () {
+        this.title = ko.observable('Remember');
+        this.message = ko.observable('the milk');
+    };
+
+    var viewmodel = new ViewModel();
+    ko.applyBindings(viewmodel);
+
+});
---
+++ Slides/examples/03_modules/require.config.js
@@ -0,0 +1,12 @@
+﻿
+requirejs.config({
+    //baseUrl: "js",
+    paths: {
+        'jquery': '../../js/jquery/jquery-1.11.1',
+        'knockout': '../../js/knockout-3.1.0/knockout.debug',
+        'domReady': '../../js/domReady',  
+    },
+    shim: {
+
+    }
+});
